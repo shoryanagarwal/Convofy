48a77ccfd0ab92cc5044aeaf472de6e06a992e0e
shoryan5135s <shoryanagarwal154@gmail.com>
2026-05-19 02:16:56 +0530

    fix socket production env

--- frontend/src/Socket/socket.js
+++ frontend/src/Socket/socket.js
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-console.log("SOCKET URL NEW BUILD:", import.meta.env.VITE_SOCKET_URL);
-
 const socket = io(import.meta.env.VITE_SOCKET_URL, {
   autoConnect: false,
 });
